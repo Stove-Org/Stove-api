281a0833f78ae0cfa4426194015e1d2c060e76f2
jinukix <jinukix@styleshare.kr>
2022-10-17 23:32:12 +0900

    login, signup request add builder

--- src/main/java/gg/stove/auth/dto/SignupRequest.java
+++ src/main/java/gg/stove/auth/dto/SignupRequest.java
@@ -2,9 +2,11 @@ package gg.stove.auth.dto;
 
 import javax.validation.constraints.NotBlank;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class SignupRequest {
 
     @NotBlank
