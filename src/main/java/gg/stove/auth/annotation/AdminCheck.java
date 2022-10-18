68977c9b7286e7fa64428949a1321b039c3c0494
jinukix <jinukix@styleshare.kr>
2022-10-18 19:45:27 +0900

    package move

--- src/main/java/gg/stove/auth/annotation/AdminCheck.java
+++ src/main/java/gg/stove/auth/annotation/AdminCheck.java
@@ -1,4 +1,4 @@
-package gg.stove.annotation;
+package gg.stove.auth.annotation;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
@@ -8,12 +8,14 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import org.springframework.security.access.annotation.Secured;
+import io.swagger.v3.oas.annotations.Operation;
 
-@Secured("ROLE_USER")
+@Secured("ROLE_ADMIN")
+@Operation(summary = "admin")
 @Target({ ElementType.METHOD, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 @Documented
-public @interface LoginCheck {
+public @interface AdminCheck {
 
 }
