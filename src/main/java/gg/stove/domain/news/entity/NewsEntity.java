608f96e06ad3bad8845afba549521fff3ea0f837
jinukix <jinukix@styleshare.kr>
2022-10-18 15:05:13 +0900

    News Update Api

--- src/main/java/gg/stove/domain/news/entity/NewsEntity.java
+++ src/main/java/gg/stove/domain/news/entity/NewsEntity.java
@@ -8,6 +8,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import gg.stove.domain.news.dto.UpdatedNewsRequest;
+import gg.stove.utils.DateUtil;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -52,5 +54,27 @@ public class NewsEntity {
         this.linkUrl = linkUrl;
         this.imageUrl = imageUrl;
         this.publishedAt = publishedAt;
+    }
+
+    public void update(UpdatedNewsRequest request) {
+        String headline = request.getHeadline();
+        if (headline != null) {
+            this.headline = headline;
+        }
+
+        String linkUrl = request.getLinkUrl();
+        if (linkUrl != null) {
+            this.linkUrl = linkUrl;
+        }
+
+        String imageUrl = request.getImageUrl();
+        if (imageUrl != null) {
+            this.imageUrl = imageUrl;
+        }
+
+        String publishedAt = request.getPublishedAt();
+        if (publishedAt != null) {
+            this.publishedAt = DateUtil.convertWithUntilMinuteString(publishedAt);
+        }
     }
 }
--- src/test/java/gg/stove/domain/news/service/NewsServiceTest.java
+++ src/test/java/gg/stove/domain/news/service/NewsServiceTest.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import gg.stove.domain.news.dto.CreateNewsRequest;
 import gg.stove.domain.news.dto.NewsViewResponse;
+import gg.stove.domain.news.dto.UpdatedNewsRequest;
 import gg.stove.domain.news.entity.NewsEntity;
 import gg.stove.domain.news.repository.NewsRepository;
 import org.junit.jupiter.api.Test;
@@ -48,6 +49,7 @@ class NewsServiceTest {
 
     @Test
     void getNewsPage() {
+        // given
         CreateNewsRequest createNewsRequest = CreateNewsRequest.builder()
             .headline("headline")
             .linkUrl("linkUrl")
@@ -67,5 +69,34 @@ class NewsServiceTest {
         then(newsViewResponse.getLinkUrl()).isEqualTo("linkUrl");
         then(newsViewResponse.getImageUrl()).isEqualTo("imageUrl");
         then(newsViewResponse.getPublishedAt()).isEqualTo("2022.10.15 오후 06:22");
+    }
+
+    @Test
+    void updateNews() {
+        CreateNewsRequest createNewsRequest = CreateNewsRequest.builder()
+            .headline("headline")
+            .linkUrl("linkUrl")
+            .imageUrl("imageUrl")
+            .publishedAt("2022-10-15 18:22")
+            .build();
+
+        newsService.createNews(createNewsRequest);
+        Long id = newsRepository.findAll().get(0).getId();
+
+        UpdatedNewsRequest updatedNewsRequest = UpdatedNewsRequest.builder()
+            .headline("headline1")
+            .imageUrl("imageUrl1")
+            .publishedAt("2021-09-14 17:10")
+            .build();
+
+        // when
+        newsService.updateNews(id, updatedNewsRequest);
+
+        // then
+        NewsEntity newsEntity = newsRepository.findAll().get(0);
+        then(newsEntity.getHeadline()).isEqualTo("headline1");
+        then(newsEntity.getLinkUrl()).isEqualTo("linkUrl");
+        then(newsEntity.getImageUrl()).isEqualTo("imageUrl1");
+        then(newsEntity.getPublishedAt()).isEqualTo(LocalDateTime.of(2021, 9,14, 17, 10 ));
     }
 }
