a02db6e77e6413caf2b80b43f777d0f58175b08c
jinukix <jinukix@styleshare.kr>
2022-10-19 14:24:30 +0900

    회원 내 정보 API 추가

--- src/main/java/gg/stove/auth/annotation/LoginCheck.java
+++ src/main/java/gg/stove/auth/annotation/LoginCheck.java
@@ -8,8 +8,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import org.springframework.security.access.annotation.Secured;
+import io.swagger.v3.oas.annotations.Operation;
 
 @Secured("ROLE_USER")
+@Operation(summary = "login")
 @Target({ ElementType.METHOD, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
--- src/main/java/gg/stove/domain/user/controller/UserController.java
+++ src/main/java/gg/stove/domain/user/controller/UserController.java
@@ -3,12 +3,17 @@ package gg.stove.domain.user.controller;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import gg.stove.auth.annotation.LoginCheck;
 import gg.stove.domain.user.dto.LoginRequest;
 import gg.stove.domain.user.dto.SignupRequest;
+import gg.stove.domain.user.dto.UserInfoView;
+import gg.stove.domain.user.model.AuthUser;
 import gg.stove.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 
@@ -16,6 +21,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
+    @LoginCheck
+    @GetMapping("/api/v1/users")
+    public UserInfoView getUserInfo(@AuthenticationPrincipal AuthUser user) {
+        return UserInfoView.from(user);
+    }
 
     @PostMapping("/api/v1/users/signup")
     @ResponseStatus(HttpStatus.CREATED)
@@ -27,5 +38,4 @@ public class UserController {
     public String login(@RequestBody @Valid LoginRequest loginRequest) {
         return userService.login(loginRequest);
     }
-
 }
