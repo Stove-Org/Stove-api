0d83a687f568f2230d7cf1cc6799fc7b8b446077
jinukix <jinukix@styleshare.kr>
2022-10-21 19:15:33 +0900

    add Team Entity

---
+++ src/main/java/gg/stove/domain/team/entity/TeamEntity.java
@@ -0,0 +1,38 @@
+package gg.stove.domain.team.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Getter
+@Table(name = "teams")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TeamEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
