4176725969a22e3b173d3ff4a00f9d220fa74b53
jinukix <jinukix@styleshare.kr>
2022-10-22 09:13:36 +0900

    swagger url 접근 제한 제거

--- src/main/java/gg/stove/auth/config/CustomAuthenticationFilter.java
+++ src/main/java/gg/stove/auth/config/CustomAuthenticationFilter.java
@@ -3,21 +3,17 @@ package gg.stove.auth.config;
 import java.io.IOException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import gg.stove.domain.user.model.AuthUser;
-import gg.stove.domain.user.entity.Authority;
 import gg.stove.domain.user.entity.UserEntity;
+import gg.stove.domain.user.model.AuthUser;
 import gg.stove.domain.user.repository.UserRepository;
 import gg.stove.utils.JwtTokenProvider;
 import lombok.NonNull;
@@ -32,43 +28,26 @@ public class CustomAuthenticationFilter extends OncePerRequestFilter {
 
     private final String TOKEN_PREFIX = "Bearer";
 
-    @Value("${swagger.secret.key}")
-    private String cookieKey;
-
-    @Value("${swagger.secret.value}")
-    private String cookieValue;
-
     @Override
     protected void doFilterInternal(
         @NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
         String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (token != null && token.startsWith(TOKEN_PREFIX)) {
+            token = token.substring(TOKEN_PREFIX.length()).strip();
+            if (jwtTokenProvider.validateToken(token)) {
+                Long userId = Long.valueOf(jwtTokenProvider.getUserIdFromJWT(token));
+                UserEntity user = useRepository.findById(userId).orElseThrow();
+                AuthUser authUser = new AuthUser(user);
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                    new UsernamePasswordAuthenticationToken(
+                        authUser,
+                        null,
+                        authUser.getAuthorities());
 
-        if (request.getServletPath().contains("swagger")) {
-            for (Cookie cookie : request.getCookies()) {
-                if (cookie.getName().equals(cookieKey) && cookie.getValue().equals(cookieValue)) {
-                    filterChain.doFilter(request, response);
-                    return;
-                }
-            }
-            throw new AccessDeniedException("swagger access denied");
-        } else {
-            if (token != null && token.startsWith(TOKEN_PREFIX)) {
-                token = token.substring(TOKEN_PREFIX.length()).strip();
-                if (jwtTokenProvider.validateToken(token)) {
-                    Long userId = Long.valueOf(jwtTokenProvider.getUserIdFromJWT(token));
-                    UserEntity user = useRepository.findById(userId).orElseThrow();
-                    AuthUser authUser = new AuthUser(user);
-                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
-                        new UsernamePasswordAuthenticationToken(
-                            authUser,
-                            null,
-                            authUser.getAuthorities());
-
-                    usernamePasswordAuthenticationToken.setDetails(
-                        new WebAuthenticationDetailsSource().buildDetails(request));
-                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-                }
+                usernamePasswordAuthenticationToken.setDetails(
+                    new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
             }
         }
 
