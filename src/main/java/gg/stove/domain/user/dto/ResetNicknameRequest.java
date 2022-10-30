003437c8e63502ccad56371fe454815114f75b84
jinukix <jinukix@styleshare.kr>
2022-10-30 21:38:43 +0900

    ResetNicknameRequest add construct

--- src/main/java/gg/stove/domain/user/dto/ResetNicknameRequest.java
+++ src/main/java/gg/stove/domain/user/dto/ResetNicknameRequest.java
@@ -1,12 +1,19 @@
 package gg.stove.domain.user.dto;
 
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResetNicknameRequest {
 
+    @NotBlank
     private String nickname;
 
 }
