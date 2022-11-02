483e367a8b218d32e1bc8112ef80b8cfc4b15297
jinukix <jinukix@styleshare.kr>
2022-11-02 23:55:53 +0900

    NewsHashtagEntity

---
+++ src/main/java/gg/stove/domain/news/repository/NewsHashtagRepository.java
@@ -0,0 +1,18 @@
+package gg.stove.domain.news.repository;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import gg.stove.domain.hashtags.entity.HashtagEntity;
+import gg.stove.domain.news.entity.NewsEntity;
+import gg.stove.domain.news.entity.NewsHashtagEntity;
+
+public interface NewsHashtagRepository extends JpaRepository<NewsHashtagEntity, Long> {
+
+    List<NewsHashtagEntity> findAllByNewsEntity(NewsEntity newsEntity);
+
+    void deleteAllByNewsEntityAndHashtag(NewsEntity newsEntity, HashtagEntity hashtagEntity);
+
+    boolean existsByHashtag(HashtagEntity hashtagEntity);
+}
