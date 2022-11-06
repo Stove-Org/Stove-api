bb0b08004cf14813f4627161dd3f37ad06f8544e
jinukix <jinukix@styleshare.kr>
2022-11-06 21:31:56 +0900

    add timezone setdefault

--- src/main/java/gg/stove/StoveApiApplication.java
+++ src/main/java/gg/stove/StoveApiApplication.java
@@ -1,5 +1,8 @@
 package gg.stove;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -12,5 +15,10 @@ public class StoveApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StoveApiApplication.class, args);
+	}
+
+	@PostConstruct
+	public void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 }
